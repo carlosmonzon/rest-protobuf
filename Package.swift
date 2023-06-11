5818e12f4dedeccff05576134d74c9f7a1e120a5
runner <runner@Mac-1686481907340.local>
2023-06-11 13:26:21 +0000

    KMM SPM package release for 0.1.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/carlosmonzon/rest-protobuf/rest-protobuf/shared-kmmbridge/0.1.1/shared-kmmbridge-0.1.1.zip"
+let remoteKotlinChecksum = "ca593907d57097357a07173441f302bca77a8c5f3f2ecb74427ea236a0cbf5c3"
 let packageName = "DataKit"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./shared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
